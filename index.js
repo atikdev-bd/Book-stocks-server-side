16ee767905e0a4501cef052a4f90819bb0cdc20a
atikdev-bd <atikdev.bd@gmail.com>
2022-11-24 03:36:11 +0600

    book categories data create and save mongodb

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const app = express();
+require("dotenv").config();
 
 ///middle ware ///
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.BOOK_DATA}:${process.env.PASSWORD}@cluster0.h7epoo8.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+const run = () => {
+  try {
+    const bookCategoriesCollection = client
+      .db("books")
+      .collection("books-categories");
 
-app.get("/", (req ,res)=>{
-    res.send('server is running')
-})
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const result = await bookCategoriesCollection.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+};
 
+run()
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-
-
-
-
-
-
-
-
-
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
 app.listen(port, () => {
   console.log(`server running on port${port}`);
